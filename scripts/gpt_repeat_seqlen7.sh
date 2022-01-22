d8894c70bc10fbd94137fb82b125eb06d9266006
Andrei <inwaves@live.com>
2022-01-22 19:47:32 +0000

    Actually put something in the script file? Maybe?

---
+++ scripts/gpt_repeat_seqlen7.sh
@@ -0,0 +1,5 @@
+python3 mingpt_maths.py --epochs=20 --seqlen=7 --ndigits=2
+python3 mingpt_maths.py --epochs=20 --seqlen=7 --ndigits=2
+python3 mingpt_maths.py --epochs=20 --seqlen=7 --ndigits=2
+python3 mingpt_maths.py --epochs=20 --seqlen=7 --ndigits=2
+python3 mingpt_maths.py --epochs=20 --seqlen=7 --ndigits=2
