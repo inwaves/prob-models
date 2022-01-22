7f8d69e12b370b838b0c5c223f89d629fd639b3e
Andrei <inwaves@live.com>
2022-01-22 21:00:02 +0000

    50 epoch repeats

---
+++ scripts/repeat_2digit50epochs.sh
@@ -0,0 +1,47 @@
+python3 mingpt_maths.py --epochs=50 --seqlen=7 --ndigits=2
+python3 mingpt_maths.py --epochs=50 --seqlen=7 --ndigits=2
+python3 mingpt_maths.py --epochs=50 --seqlen=7 --ndigits=2
+python3 mingpt_maths.py --epochs=50 --seqlen=7 --ndigits=2
+python3 mingpt_maths.py --epochs=50 --seqlen=7 --ndigits=2
+
+python3 mingpt_maths.py --epochs=50 --seqlen=8 --ndigits=2
+python3 mingpt_maths.py --epochs=50 --seqlen=8 --ndigits=2
+python3 mingpt_maths.py --epochs=50 --seqlen=8 --ndigits=2
+python3 mingpt_maths.py --epochs=50 --seqlen=8 --ndigits=2
+python3 mingpt_maths.py --epochs=50 --seqlen=8 --ndigits=2
+
+python3 mingpt_maths.py --epochs=50 --seqlen=9 --ndigits=2
+python3 mingpt_maths.py --epochs=50 --seqlen=9 --ndigits=2
+python3 mingpt_maths.py --epochs=50 --seqlen=9 --ndigits=2
+python3 mingpt_maths.py --epochs=50 --seqlen=9 --ndigits=2
+python3 mingpt_maths.py --epochs=50 --seqlen=9 --ndigits=2
+
+python3 mingpt_maths.py --epochs=50 --seqlen=10 --ndigits=2
+python3 mingpt_maths.py --epochs=50 --seqlen=10 --ndigits=2
+python3 mingpt_maths.py --epochs=50 --seqlen=10 --ndigits=2
+python3 mingpt_maths.py --epochs=50 --seqlen=10 --ndigits=2
+python3 mingpt_maths.py --epochs=50 --seqlen=10 --ndigits=2
+
+python3 rnn_maths.py --epochs=50 --seqlen=7 --ndigits=2
+python3 rnn_maths.py --epochs=50 --seqlen=7 --ndigits=2
+python3 rnn_maths.py --epochs=50 --seqlen=7 --ndigits=2
+python3 rnn_maths.py --epochs=50 --seqlen=7 --ndigits=2
+python3 rnn_maths.py --epochs=50 --seqlen=7 --ndigits=2
+
+python3 rnn_maths.py --epochs=50 --seqlen=8 --ndigits=2
+python3 rnn_maths.py --epochs=50 --seqlen=8 --ndigits=2
+python3 rnn_maths.py --epochs=50 --seqlen=8 --ndigits=2
+python3 rnn_maths.py --epochs=50 --seqlen=8 --ndigits=2
+python3 rnn_maths.py --epochs=50 --seqlen=8 --ndigits=2
+
+python3 rnn_maths.py --epochs=50 --seqlen=9 --ndigits=2
+python3 rnn_maths.py --epochs=50 --seqlen=9 --ndigits=2
+python3 rnn_maths.py --epochs=50 --seqlen=9 --ndigits=2
+python3 rnn_maths.py --epochs=50 --seqlen=9 --ndigits=2
+python3 rnn_maths.py --epochs=50 --seqlen=9 --ndigits=2
+
+python3 rnn_maths.py --epochs=50 --seqlen=10 --ndigits=2
+python3 rnn_maths.py --epochs=50 --seqlen=10 --ndigits=2
+python3 rnn_maths.py --epochs=50 --seqlen=10 --ndigits=2
+python3 rnn_maths.py --epochs=50 --seqlen=10 --ndigits=2
+python3 rnn_maths.py --epochs=50 --seqlen=10 --ndigits=2
