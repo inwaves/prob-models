0008b9a60cd9509025b25102b5b2359f7ecc981c
Andrei <inwaves@live.com>
2022-01-22 19:46:52 +0000

    Look furthewr into flakey experiments

---
+++ scripts/rnn_repeat_seqlen6.sh
@@ -0,0 +1,5 @@
+python3 rnn_maths.py --epochs=20 --seqlen=6 --ndigits=2
+python3 rnn_maths.py --epochs=20 --seqlen=6 --ndigits=2
+python3 rnn_maths.py --epochs=20 --seqlen=6 --ndigits=2
+python3 rnn_maths.py --epochs=20 --seqlen=6 --ndigits=2
+python3 rnn_maths.py --epochs=20 --seqlen=6 --ndigits=2
